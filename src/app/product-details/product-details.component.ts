15c40cd4b5eedc1975204a012384d46f9a43dcb9
Feng <zhang7777@gmail.com>
2023-04-02 22:54:32 +0200

    init commit

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Product, products } from '../products';
+import { ActivatedRoute } from '@angular/router';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-product-details',
+  templateUrl: './product-details.component.html',
+  styleUrls: ['./product-details.component.css']
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+  constructor(private route: ActivatedRoute,
+    private cartService: CartService){
+
+  }
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdParams = Number(routeParams.get('productId'));
+
+    this.product = products.find(x=>x.id === productIdParams);
+  }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    window.alert('Your prouduct has been added to the cart');
+  }
+
+}
